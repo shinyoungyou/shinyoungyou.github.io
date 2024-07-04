f7c75948ded64bc3b46a0265d1ebdfc32f63261f
Shiloh <austyn.you@gmail.com>
2024-07-04 10:59:22 +0900

    add txt

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,9 +17,9 @@ export default function IndexPage() {
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
         <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
-        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
+        <AboutSection sectionId="about" heading="About Shinyoung" />
+        <InterestsSection sectionId="details" heading="Technical Skills" />
+        <ProjectsSection sectionId="features" heading="Team/Personal Projects" />
         <ContactSection sectionId="github" heading="Issues?" />
       </Page>
     </>
